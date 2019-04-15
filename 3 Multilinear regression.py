a2f782f779071a42e94dea1fe19b54d6d8a48f23
metintorunoglu <45499397+metintorunoglu@users.noreply.github.com>
2019-04-15 20:14:20 +0300

    Update 3 Multilinear regression.py

--- 3 Multilinear regression.py
+++ 3 Multilinear regression.py
@@ -9,7 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #import the dataset
-dataset=pd.read_csv('50_Startups.csv')
+dataset=pd.read_csv('dataset.csv')
 X=dataset.iloc[:, :-1].values
 y=dataset.iloc[:, 4].values
 
